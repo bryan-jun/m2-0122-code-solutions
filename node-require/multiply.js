49330d13ce54bc9ab0e9ae7c51f01b7a87013679
Bryan Jun <bryjun@gmail.com>
2022-03-02 05:30:46 +0000

    changed parseInt to Number()

--- node-require/multiply.js
+++ node-require/multiply.js
@@ -1,6 +1,6 @@
 module.exports = function multiply(num1, num2) {
-  const number1 = parseInt(num1);
-  const number2 = parseInt(num2);
+  const number1 = Number(num1);
+  const number2 = Number(num2);
   const answer = number1 * number2;
 
   return answer;
