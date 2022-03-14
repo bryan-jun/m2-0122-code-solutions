52d87e9f538025f166edbad87c86bc7b32c60824
Bryan Jun <bryjun@gmail.com>
2022-03-14 17:32:55 +0000

    removed redundant stringify and fixed update js

--- node-notes-cli/add.js
+++ node-notes-cli/add.js
@@ -6,7 +6,6 @@ module.exports = function add(addition) {
   var newKey = data.nextId;
   data.notes[newKey] = newEntry;
   data.nextId = data.nextId + 1;
-  JSON.stringify(data, null, 2);
   fs.writeFile('data.json', JSON.stringify(data), function (err) {
     if (err) throw err;
   });
